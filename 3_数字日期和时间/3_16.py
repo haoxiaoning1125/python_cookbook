c79a0a9aa3d86fd00856c0c5a41ab71f86cb4b67
haoxiaoning <1977835153@qq.com>
2019-08-09 11:09:21 +0800

    4_1.py

--- 3_数字日期和时间/3_16.py
+++ 3_数字日期和时间/3_16.py
@@ -4,6 +4,26 @@
 结合时区的日期操作
 """
 
+from datetime import datetime, timedelta
+from pytz import timezone, utc
+
+
 if __name__ == '__main__':
-    print 'sss'
-    pass
+    d = datetime(2012, 12, 21, 9, 30, 0)
+    print 'd: {}'.format(d)
+
+    # Localize the date for Chicago
+    central = timezone('US/Central')
+    loc_d = central.localize(d)
+    print 'loc_d: {}'.format(loc_d)
+
+    # Convert to Bangalore time
+    bang_d = loc_d.astimezone(timezone('Asia/Kolkata'))
+    print 'bang_d: {}'.format(bang_d)
+
+    # 处理本地化日期的通常的策略先将所有日期转换为 UTC 时间
+    print ('loc_d: ', loc_d)
+    utc_d = loc_d.astimezone(utc)
+    print ('utc_d: ', utc_d)
+    later_utc = utc_d + timedelta(minutes=30)
+    print ('later_utc: ', later_utc.astimezone(central))
---
+++ 4_迭代器与生成器/4_1.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+
+"""
+手动遍历迭代器
+"""
+
+if __name__ == '__main__':
+    pass
