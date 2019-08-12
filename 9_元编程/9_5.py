89812eddeb99a951c04434aa6f50f859ec90fbab
haoxiaoning <1977835153@qq.com>
2019-08-12 20:09:57 +0800

    ls

--- 9_元编程/9_5.py
+++ 9_元编程/9_5.py
@@ -1,11 +1,19 @@
 # coding:utf-8
 
 """
-定义一个带参数的装饰器
+可自定义属性的装饰器
 """
 
-from functools import wraps
+
+from functools import wraps, partial
 import logging
+
+
+def attach_wrapper(obj, func=None):
+    if func is None:
+        return partial(attach_wrapper, obj)
+    setattr(obj, func.__name__, func)
+    return func
 
 
 def logged(level, name=None, message=None):
@@ -13,24 +21,12 @@ def logged(level, name=None, message=None):
         logname = name if name else func.__module__
         log = logging.getLogger(logname)
         logmsg = message if message else func.__name__
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             log.log(level, logmsg)
             return func(*args, **kwargs)
-        return wrapper
-    return decorate
-
-
-@logged(logging.DEBUG)
-def add(x, y):
-    return x + y
-
-
-@logged(logging.CRITICAL, 'example')
-def spam():
-    print 'spam'
 
 
 if __name__ == '__main__':
-    spam()
-    add(1, 2)
+    pass
